7d47705e5cbbfa0cbeba97b163e1fc477e18642e
dimasgda <dimasgilang0803@gmail.com>
2025-08-09 22:48:57 +0700

    day(1):  two sum

---
+++ two-sum.ts
@@ -0,0 +1,25 @@
+// Time Complexity: O(n^2), because we are using two nested loops to check each pair of numbers.
+function twoSumBruteForce(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+// Time Complexity: O(n), because we are using a single loop and a hash map to store the indices of the numbers.
+
+function twoSumMap(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
